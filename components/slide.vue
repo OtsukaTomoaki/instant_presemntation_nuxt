b4618a8ff1e5cbcbe49a7e566a4747665dbf062c
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2024-01-02 16:08:11 +0900

    無駄なコメント削除

--- components/slide.vue
+++ components/slide.vue
@@ -40,43 +40,4 @@ onMounted(() => {
     console.error(reason)
   })
 })
-// import { getDocument } from 'pdfjs-dist/lib/pdf';
-
-// const pdfViewer = ref(null);
-
-// onMounted(async () => {
-//   // PDF.jsのワーカースクリプトのパスを設定
-//   pdfjsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/[PDF.jsのバージョン]/pdf.worker.js';
-
-//   // PDFをロード
-//   const loadingTask = pdfjsLib.getDocument('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf');
-//   try {
-//     const pdf = await loadingTask.promise;
-
-//     // 最初のページを取得
-//     const page = await pdf.getPage(1);
-
-//     // ビューポートをセットアップ
-//     const scale = 1.5;
-//     const viewport = page.getViewport({ scale });
-
-//     // Canvasを作成してPDFをレンダリング
-//     const canvas = document.createElement('canvas');
-//     const context = canvas.getContext('2d');
-//     canvas.height = viewport.height;
-//     canvas.width = viewport.width;
-
-//     // CanvasをDOMに追加
-//     pdfViewer.value.appendChild(canvas);
-
-//     // PDFページをCanvasにレンダリング
-//     const renderContext = {
-//       canvasContext: context,
-//       viewport: viewport
-//     };
-//     await page.render(renderContext).promise;
-//   } catch (error) {
-//     console.error('PDFのロードに失敗しました:', error);
-//   }
-// });
 </script>
