f4387e9b626c6dfd9bbc19134a56613515ec226e
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2024-01-02 20:40:04 +0900

    次へ、前へボタンの配置

--- components/slide.vue
+++ components/slide.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="Container">
     <div ref="renderPdfSpace"></div>
+    <div class="PrevNextButton">
+      <button class="PrevButton" @click="prevPage">Prev</button>
+      <button class="NextButton" @click="nextPage">Next</button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +16,19 @@ import * as pdfjsWorker from 'pdfjs-dist/legacy/build/pdf.worker.min.mjs'
 PDFJS.GlobalWorkerOptions.workerSrc = pdfjsWorker
 
 const renderPdfSpace = ref(null)
+const currentPage = ref(1)
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+  }
+}
+
+const nextPage = () => {
+  if (currentPage.value < 5) {
+    currentPage.value++
+  }
+}
 
 onMounted(() => {
   const loadingTask = PDFJS.getDocument('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')
@@ -21,7 +38,7 @@ onMounted(() => {
     const numPages = pdf.numPages
     console.log('numPages', numPages)
     // 1ページ目を取得
-    pdf.getPage(1).then((page) => {
+    pdf.getPage(currentPage.value).then((page) => {
       console.log('Page loaded')
       const scale = 1.5
       const viewport = page.getViewport({ scale: scale })
@@ -44,6 +61,10 @@ onMounted(() => {
     console.error(reason)
   })
 })
+
+const renderPdf = () => {
+  console.log('renderPdf')
+}
 </script>
 
 <style scoped>
@@ -53,5 +74,14 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.PrevNextButton {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
